9e2dd2e2805f8df1abcb4a7b92805614263849a1
panjikrisnayasabfi <panjikrisnayasabfi@gmail.com>
2024-07-29 23:51:50 +0700

    feat: create my audio handler

---
+++ lib/app/services/my_audio_handler.dart
@@ -0,0 +1,66 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:just_audio/just_audio.dart';
+
+Future<AudioHandler> initAudioService() async {
+  return await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'com.quran.app.quran_app',
+      androidNotificationChannelName: 'Audio Playback',
+      androidNotificationOngoing: true,
+    ),
+  );
+}
+
+class MyAudioHandler extends BaseAudioHandler with SeekHandler {
+  final _player = AudioPlayer();
+
+  MyAudioHandler() {
+    _player.playbackEventStream.map(_transformEvent).pipe(playbackState);
+  }
+
+  @override
+  Future<void> play() => _player.play();
+
+  @override
+  Future<void> pause() => _player.pause();
+
+  @override
+  Future<void> seek(Duration position) => _player.seek(position);
+
+  @override
+  Future<void> stop() => _player.stop();
+
+  @override
+  Future<void> addQueueItem(MediaItem mediaItem) async {
+    this.mediaItem.add(mediaItem);
+
+    await _player.setAudioSource(
+      AudioSource.uri(Uri.parse(mediaItem.id)),
+      initialPosition: Duration.zero,
+    );
+  }
+
+  PlaybackState _transformEvent(PlaybackEvent event) {
+    return PlaybackState(
+      controls: [
+        if (_player.playing) MediaControl.pause else MediaControl.play,
+        MediaControl.stop,
+      ],
+      androidCompactActionIndices: const [0, 1, 3],
+      processingState: const {
+            ProcessingState.idle: AudioProcessingState.idle,
+            ProcessingState.loading: AudioProcessingState.loading,
+            ProcessingState.buffering: AudioProcessingState.buffering,
+            ProcessingState.ready: AudioProcessingState.ready,
+            ProcessingState.completed: AudioProcessingState.completed,
+          }[_player.processingState] ??
+          AudioProcessingState.loading,
+      playing: _player.playing,
+      updatePosition: _player.position,
+      bufferedPosition: _player.bufferedPosition,
+      speed: _player.speed,
+      queueIndex: event.currentIndex,
+    );
+  }
+}
